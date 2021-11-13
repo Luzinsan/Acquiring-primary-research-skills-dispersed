5ea2518e17be42c4f8b014e12616cb6e9f7ff611
DarkMessiah5 <pavekum@mail.ru>
2021-11-14 03:16:22 +0700

    Update canvas.cpp

--- ZKL/canvas.cpp
+++ ZKL/canvas.cpp
@@ -20,19 +20,25 @@
     //QGraphicsScene* scene = new QGraphicsScene;
     //this->addItem(&item); */
 
+/* ===============================================================
+ *
+ * ========== НИЖЕ КОНСТРУКТОР И ДЕСТРУКТОР ХОЛСТА ===============
+ *
+ * ===============================================================
+ */
+
 Canvas::Canvas(QObject *parent) : QGraphicsScene(parent)
 {
     //this->set
     //rubberBand = nullptr;
     rubberBand = new QRubberBand(QRubberBand::Rectangle);
-    pen->setColor(Qt::black);
-    pen->setStyle(Qt::SolidLine);
-    pen->setCapStyle(Qt::RoundCap);
-    pen->setWidth(1);
+    pen = new QPen(Qt::black, 1, Qt::SolidLine, Qt::SquareCap);
 }
 
 Canvas::~Canvas()
 {
+    delete rubberBand;
+    delete pen;
 }
 
 void Canvas::loadPicture(QString path)
@@ -44,19 +50,41 @@ void Canvas::loadPicture(QString path)
     addItem(&item);
 }
 
+/* ===============================================================
+ *
+ * ========== НИЖЕ РЕДАКТИРОВАНИЕ ВЫДЕЛЕННОЙ ОБЛАСТИ =============
+ *
+ * ===============================================================
+ */
+
+void Canvas::copySceneAsIMG()
+{
+    this->clearSelection();                                                  // Selections would also render to the file
+    this->setSceneRect(this->itemsBoundingRect());                          // Re-shrink the scene to it's bounding contents
+    QImage image(this->sceneRect().size().toSize(), QImage::Format_ARGB32);  // Create the image with the exact size of the shrunk scene
+    canvasCopy = image;
+    canvasCopy.fill(Qt::transparent);
+}
+
+void Canvas::cropImage()
+{
+    copySceneAsIMG();
+
+    croppedImage = canvasCopy.copy(highlighterCoords);
+}
+
+
+/* ===============================================================
+ *
+ * ========== НИЖЕ ФУНКЦИИ ДЛЯ НАСТРОЙКИ ИНСТРУМЕНТОВ ============
+ *
+ * ===============================================================
+ */
+
 void Canvas::setChosenInstrument(int id)
 {
     chosenInstrument = id;
 }
-
-/*void Canvas::copySceneAsIMG()
-{
-    this->clearSelection();                                                  // Selections would also render to the file
-    //this->setSceneRect(this->itemsBoundingRect());                          // Re-shrink the scene to it's bounding contents
-    //QImage image(this->sceneRect().size().toSize(), QImage::Format_ARGB32);  // Create the image with the exact size of the shrunk scene
-//    canvasCopy.copy(0, 0, this->width(), this->height());
-    canvasCopy.fill(Qt::transparent);
-} */
 
 void Canvas::setSize(int size)
 {
@@ -64,6 +92,12 @@ void Canvas::setSize(int size)
 }
 
 
+/* ===============================================================
+ *
+ * ===== НИЖЕ ФУНКЦИИ ДЛЯ ОТОБРАЖЕНИЯ ИНФОРМАЦИИ НА ХОЛСТЕ =======
+ *
+ * ===============================================================
+ */
 
 void Canvas::drawLine(int x, int y)
 {
@@ -75,29 +109,149 @@ void Canvas::drawLine(int x, int y)
             *pen);
 }
 
+// =============== ТРЕУГОЛЬНИК ===================
+/*
+ * addLine(x1, y2, (x1 + x2) / 2, y1, *pen);
+
+   addLine(x2, y2, (x1 + x2) / 2, y1, *pen);
+
+   addLine(x1, y2, x2, y2, *pen);
+ */
+
+
+// =============== ПРЯМОУГОЛЬНЫЙ ТРЕУГОЛЬНИК ===================
+/*
+ * addLine(x1, y2, x1, y1, *pen);
+
+   addLine(x1, y2, x2, y2, *pen);
+
+   addLine(x1, y1, x2, y2, *pen);
+ */
+
+
+// =============== ТРАПЕЦИЯ ===================
+/*
+ * qreal len;
+
+            if ((x1 >= 0  && x2 <= 0) || (x1 <= 0 && x2 >= 0))
+                len = (abs(x1)+abs(x2)) * coeff;
+            else
+                len = (abs(x2) - abs(x1)) * coeff;
+
+            addLine(x1, y2, x2, y2, *pen);
+
+            addLine(x1, y2, x1 + len, y1, *pen);
+
+            addLine(x1 + len, y1, x2 - len, y1, *pen);
+
+            addLine(x2 - len, y1, x2, y2, *pen);
+ */
+
+
+// =============== РОМБ ===================
+/*
+ * qreal cW, cH;
+
+            if ((x1 >= 0  && x2 <= 0) || (x1 <= 0 && x2 >= 0))
+                cW = abs((abs(x1) + abs(x2))) / 2 + x1;
+            else
+                cW = abs((abs(x2) - abs(x1))) / 2 + x1;
+
+            if ((y1 >= 0  && y2 <= 0) || (y1 <= 0 && y2 >= 0))
+                cH = abs((abs(y1) + abs(y2))) / 2 + y1;
+            else
+                cH = abs((abs(y2) - abs(y1))) / 2 + y1;
+
+            addLine(x1, cH, cW, y1, *pen);
+
+            addLine(cW, y1, x2, cH, *pen);
+
+            addLine(x2, cH, cW, y2, *pen);
+
+            addLine(cW, y2, x1, cH, *pen);
+
+ */
+
+/* ===============================================================
+ *
+ * ===================== НИЖЕ MOUSE EVENT'Ы ======================
+ *
+ * ===============================================================
+ */
+
 void Canvas::mouseReleaseEvent(QGraphicsSceneMouseEvent *event)
 {
     Q_UNUSED(event);
+
+    highlighterCoords.setCoords(mouseOnPressXPosition, mouseOnPressYPosition,
+                                event->screenPos().x(),event->screenPos().y());
+
+
+
     mouseOnStartMoveXPosition = -1;
     mouseOnStartMoveYPosition = -1;
     //rubberBand->hide();
+
+
 }
 
 void Canvas::mousePressEvent(QGraphicsSceneMouseEvent *event)
 {
-    mouseOnPressXPosition = event->screenPos().x();
-    mouseOnPressYPosition = event->screenPos().y();
+
+    mouseOnPressXPosition = event->scenePos().x();
+    mouseOnPressYPosition = event->scenePos().y();
+
+    qreal coeff = 0.25, uCoeff = 0.35;
+
+    if (mouseOnEndPressXPosition == -2)
+    {
+        x1 = event->scenePos().x();
+        y1 = event->scenePos().y();
+        mouseOnEndPressXPosition = -1;
+        mouseOnEndPressYPosition = -1;
+    } else
+        if (mouseOnEndPressXPosition == -1)
+        {
+            x2 = event->scenePos().x();
+            y2 = event->scenePos().y();
+
+            qreal shift, uShift;
+
+                        if ((x1 >= 0  && x2 <= 0) || (x1 <= 0 && x2 >= 0))
+                            shift = ((abs(x1) + abs(x2))) * coeff;
+                        else
+                            shift = abs((abs(x2) - abs(x1))) * coeff;
+
+                        if ((y1 >= 0  && y2 <= 0) || (y1 <= 0 && y2 >= 0))
+                            uShift = abs((abs(y1) + abs(y2))) * uCoeff;
+                        else
+                            uShift = abs((abs(y2) - abs(y1))) * uCoeff;
+
+                        addLine(x1 + shift, y2, x2 - shift, y2, *pen);
+
+                        addLine(x2 - shift, y2, x2, y1 + uShift, *pen);
+
+                        addLine(x2, y1 + uShift, x1 + shift, y1, *pen);
+
+                        addLine(x1 + shift, y1, x1, y1 + uShift, *pen);
+
+                        addLine(x1, y1 + uShift, x1 + shift, y2, *pen);
+
+            mouseOnEndPressXPosition = -2;
+            mouseOnEndPressYPosition = -2;
+        }
+
 
     printf("x = %d, y = %d\n", mouseOnPressXPosition, mouseOnPressYPosition);
 
 
-    addEllipse(event->scenePos().x(),
+    /*addEllipse(event->scenePos().x(),
                event->scenePos().y(),
                pen->width(),
                pen->width(),
-               *pen);
+               *pen); */
     // Сохраняем координаты точки нажатия
-    previousPoint = event->scenePos(); Qt::GlobalColor n;
+    previousPoint = event->scenePos();
 
    origin = event->pos();
         //if (!rubberBand)
@@ -121,6 +275,9 @@ void Canvas::mouseMoveEvent(QGraphicsSceneMouseEvent *event)
         case 1: // Ластик
             break;
         case 2: // Прямоугольное выделение объекта
+                // Надо еще предусмотреть вариант, когда
+                // текущая позиция имеет меньшие значения координат,
+                // чем начальные
             rubberBand->setGeometry(mouseOnStartMoveXPosition, mouseOnStartMoveYPosition,
                                     event->screenPos().x() - mouseOnStartMoveXPosition,
                                     event->screenPos().y() - mouseOnStartMoveYPosition); //QRect(origin.toPoint(), event->scenePos().toPoint()).normalized()
